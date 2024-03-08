eaa08b8193317264966b0850d9501ae12acb53b1
weedeej <work.deeej@gmail.com>
2024-03-08 13:59:14 +0800

    Added Reject unauthorized in redis tls

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,9 @@ export const redis =
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT),
     password: process.env.REDIS_PASSWORD,
+    tls: {
+      rejectUnauthorized: false
+    }
   });
 
 const fastify = Fastify({
